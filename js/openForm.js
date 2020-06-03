8af7146a0098c35d7094771e1556a1bbab916b4d
ketnoitinhnguyen <66156989+ketnoitinhnguyen@users.noreply.github.com>
2020-06-03 20:32:22 +0700

    Add files via upload

---
+++ js/openForm.js
@@ -0,0 +1,13 @@
+
+document.getElementById('sign').addEventListener("click", function () {
+    document.querySelector('.container').style.display = "block";
+});
+document.querySelector('.close').addEventListener('click', function () {
+    document.querySelector('.container').style.display = "none";
+});
+document.getElementById('login').addEventListener("click", function () {
+    document.querySelector('.container2').style.display = "block";
+});
+document.querySelector('.off').addEventListener('click', function () {
+    document.querySelector('.container2').style.display = "none";
+});
