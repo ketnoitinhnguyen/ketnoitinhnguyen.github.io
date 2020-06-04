bd7caa8d97667d26aa08c1afc0b5d61bf242433c
ketnoitinhnguyen <66156989+ketnoitinhnguyen@users.noreply.github.com>
2020-06-04 21:56:18 +0700

    update

--- js/popup.js
+++ js/popup.js
@@ -13,18 +13,6 @@ btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
 
 var modal2 = document.getElementById("myModal2");
 
@@ -32,10 +20,26 @@ var modal2 = document.getElementById("myModal2");
 var btn2 = document.getElementById("myBtn2");
 
 // Get the <span> element that closes the modal
-var span2 = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("off")[0];
 
 // When the user clicks the button, open the modal 
 btn2.onclick = function() {
   modal2.style.display = "block";
 }
 
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if ((event.target == modal) || (event.target == modal2)) {
+    modal.style.display = "none";
+    modal2.style.display = "none";
+  }
+}
