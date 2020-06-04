c7b4a05f30a94bcb988b36574489722b9c0bd88a
ketnoitinhnguyen <66156989+ketnoitinhnguyen@users.noreply.github.com>
2020-06-04 21:55:22 +0700

    update

--- js/openForm.js
+++ js/openForm.js
@@ -5,7 +5,7 @@ document.getElementById('myBtn').addEventListener("click", function () {
 document.querySelector('.close').addEventListener('click', function () {
     document.querySelector('.container').style.display = "none";
 });
-document.getElementById('login').addEventListener("click", function () {
+document.getElementById('myBtn2').addEventListener("click", function () {
     document.querySelector('.container2').style.display = "block";
 });
 document.querySelector('.off').addEventListener('click', function () {
